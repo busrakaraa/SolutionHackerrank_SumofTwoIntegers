b99576ffecea94f2186d2dda9260009ac265ffe2
busrakaraa <39573363+busrakaraa@users.noreply.github.com>
2020-11-22 16:56:37 +0300

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionHackerrank_SumofTwoIntegers
+{
+    class Sum
+    {
+       
+
+        static void Main(String[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+
+        static int solveMeFirst(int a, int b)
+        {
+            int r = a + b;
+            return r;
+        }
+
+
+    }
+}
